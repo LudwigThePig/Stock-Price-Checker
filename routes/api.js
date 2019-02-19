0b7072f2a271496745ee3dd2aab0d57f5c727507
Morgan Galvin <morgangalvin@yahoo.com>
2019-02-20 10:00:47 +1300

    Began refactor of api, to accept multiple inputs
    Symbols are stored in an array and each item in the array is run through the function db, which gets the number of likes from the database and price from the iex api. Values are stored in the obj object and pushed to the response array. Response array is returned to the user

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,10 @@
 'use strict';
-
+/*
+  Symbols are held in an array.
+  Each member of the array is given to the db function and returns an object with symbol and likes
+  Object is stored in response array
+  
+*/
 const expect = require('chai').expect;
 const fetch = require('node-fetch');
 const Stock = require('../models/stock-model');
@@ -9,37 +14,49 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
     let like = req.query.like;
-    const symb = req.query.symbol;    
-    const price = fetch(`https://api.iextrading.com/1.0/stock/${symb}/price`).then(res => res.json());
-
-      Stock.findOne({symbol: symb})
-      .then(stock => {
-        if (!stock){
-          let stock = new Stock({
-            symbol: symb,
-            likes: like ? 1 : 0,
-          });
-          stock.save();            
-        } else {
-          if (like === 'true'){
-            like = stock.likes + 1;
-            Stock.update({symbol: symb}, {likes: stock.likes + 1})
-          } else {
-            like = stock.likes;
-          }
-        }
-      })
-      .catch(err => console.log(err));
-
-    price.then(data =>{ 
-      let response = {
-        symbol: symb,
-        price: data,
-        likes: like
+    let symb;  
+    let response = [];
+    if (req.query.symboltwo){
+      symb = [req.query.symbolone, req.query.symboltwo]
+    } else { 
+      symb = [req.query.symbolone];
+    }
+        
+    const db = (symb)=>{
+      let obj = {
+        symbol: symb
       }
-      console.log(response);
-      res.json(response)
-    })
-    .catch(err => console.log(err));
+      
+      //Just for getting and setting likes
+      Stock.findOne({symbol: symb})
+        .then(stock => {
+          if (!stock){
+            let stock = new Stock({
+              symbol: symb,
+              likes: like ? 1 : 0,
+            });
+            stock.save();            
+          } else {
+            if (like === 'true'){
+              obj.like = stock.likes + 1;
+              Stock.update({symbol: symb}, {likes: stock.likes + 1})
+            } else {
+              like = stock.likes;
+            }
+          }
+        })
+        .catch(err => console.log(err));
+      
+      //For getting price
+      const price = fetch(`https://api.iextrading.com/1.0/stock/${symb}/price`).then(res => res.json());
+      price.then(data=>{
+        obj.price = data;
+      })
+      response.push(obj);
+    }
+    
+    symb.forEach(x => db(x));
+    setTimeout(_=>res.json(response), 1000)
+    
   })
 };
