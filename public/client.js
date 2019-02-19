28a5b41f76c089ce16a7b8395d79940b921e8324
Morgan Galvin <morgangalvin@yahoo.com>
2019-02-20 09:32:47 +1300

    Set up client side of compare stock function
    Handled event handler, updated request url for two stocks, and set up fetch request to log data

--- public/client.js
+++ public/client.js
@@ -5,7 +5,7 @@ let results = [];
 const fetchers = {
   getOneStock: function(stock, like = false){
     
-    const request = `/api/stock-prices?symbol=${stock}&like=${like}`
+    const request = `/api/stock-prices?symbol=${stock}&like=${like}`;
     
     return fetch(request)
       .then(res => res.json())
@@ -13,7 +13,11 @@ const fetchers = {
       .catch(err => console.log(err));
   },
   getTwoStocks: function(stockOne, stockTwo, like = false){
-  
+  const request = `/api/stock-prices?symbolone=${stockOne}&symboletwo=${stockTwo}&like=${like}`;
+    return fetch(request)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
   }
 }
 
@@ -26,18 +30,21 @@ const listeners = ()=>{
       compareFieldOne = document.getElementsByName("stock")[1],
       compareFieldTwo = document.getElementsByName("stock")[2],
       likeTwo = document.getElementsByName("like")[1];
-
   
   formOne.addEventListener('submit', function(e){
     e.preventDefault();
     fetchers.getOneStock(singleField.value, likeOne.checked);
     setTimeout(_=>renderer(results), 1000);
-    
+    singleField.value = '';
+    likeOne.checked = false;    
   })
+  
   formTwo.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log(compareFieldOne.value);
-    console.log(compareFieldTwo.value);
+    fetchers.getTwoStocks(compareFieldOne.value, compareFieldTwo.value, likeTwo.checked);
+    compareFieldOne.value = '';
+    compareFieldTwo.value = '';
+    likeTwo.checked = false;
   })
 }
 
@@ -50,11 +57,11 @@ const renderer = (results)=>{
     const price = document.createElement('p');
     const likes = document.createElement('p');
     
-    symbol.innerText = results[i].symbol;
-    price.innerText = results[i].price;
-    likes.innerText = results[i].likes;
+    symbol.innerText = `${results[i].symbol.toUpperCase()}`;
+    price.innerText = `$${results[i].price}usd`;
+    likes.innerText = `Likes: ${results[i].likes}`;
     
-    div.setAttribute('id', results[i].symbol)
+    div.setAttribute('class', 'stock-box')
     
     div.appendChild(symbol);
     div.appendChild(price);
@@ -64,8 +71,7 @@ const renderer = (results)=>{
       domNode.removeChild(domNode.firstChild);
     }
     domNode.appendChild(div);
-  }
-  
+  } 
 }
 
 window.onload = init();
