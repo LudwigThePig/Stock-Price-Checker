7c45c31b6846fd9a23a21f2d6ad4efcadb373715
Morgan Galvin <morgangalvin@yahoo.com>
2019-02-21 08:09:26 +1300

    Cleaned up the error message and mess from the last commit
    Likes are no longer working... TODO!

--- routes/api.js
+++ routes/api.js
@@ -14,33 +14,36 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
     let like = req.query.like;
-    let symb;  
+    let symb = [req.query.symbolone];
     let response = [];
-    if (req.query.symboltwo){
-      symb = [req.query.symbolone, req.query.symboltwo]
-    } else { 
-      symb = [req.query.symbolone];
+    
+    if (req.query.symboltwo !== undefined){
+      symb.push(req.query.symboltwo);
+      console.log('two symbs');
     }
         
-    const db = (symb)=>{
+    const db = (symbol)=>{
       let obj = {
-        symbol: symb
+        symbol: symbol,
+        likes: 0,
+        price: 0
       }
       
       //Just for getting and setting likes
-      Stock.findOne({symbol: symb})
+      Stock.findOne({symbol: symbol})
         .then(stock => {
           if (!stock){
             let stock = new Stock({
-              symbol: symb,
+              symbol: symbol,
               likes: like ? 1 : 0,
             });
             stock.save();            
           } else {
             if (like === 'true'){
               obj.like = stock.likes + 1;
-              Stock.update({symbol: symb}, {likes: stock.likes + 1})
+              Stock.update({symbol: symbol}, {likes: stock.likes + 1})
             } else {
+              console.log('line 46');
               like = stock.likes;
             }
           }
@@ -48,15 +51,16 @@ module.exports = function (app) {
         .catch(err => console.log(err));
       
       //For getting price
-      const price = fetch(`https://api.iextrading.com/1.0/stock/${symb}/price`).then(res => res.json());
-      price.then(data=>{
-        obj.price = data;
+      const price = fetch(`https://api.iextrading.com/1.0/stock/${obj.symbol}/price`).then(res => res.json());
+      price.then( data =>{
+        obj.price = data; 
+        response.push(obj);
       })
-      response.push(obj);
-    }
+        .catch( err => console.log(err) );
+    }//end db function
     
     symb.forEach(x => db(x));
-    setTimeout(_=>res.json(response), 1000)
+    setTimeout(_=>res.json(response), 2000); //hack and slash method of asynch/await 
     
-  })
-};
+  })//end get function
+}//end module.exports
