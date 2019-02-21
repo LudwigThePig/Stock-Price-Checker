91b8bf3568da4b7a61ff32c2409cfbeb6e4e375b
Morgan Galvin <morgangalvin@yahoo.com>
2019-02-22 07:23:18 +1300

    Completed functional testing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -20,29 +20,73 @@ suite('Functional Tests', function() {
       test('1 stock', function(done) {
        chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: 'goog'})
+        .query({symbolone: 'goog', like: false})
         .end(function(err, res){
-          
-          //complete this one too
-          
+          assert.equal(res[0].symbol, "goog");
+          assert.isString(res[0].symbol, "Symbol one should be a string");
+          assert.isNumber(res[0].price, "Price should be a number");  
           done();
         });
       });
       
       test('1 stock with like', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({symbolone: 'ge', like: true})
+        .end(function(err, res){
+            assert.equal(res[0].symbol, "ge");
+            assert.isString(res[0].symbol, "Symbol one should be a string");
+            assert.isNumber(res[0].price, "Price should be a number");
+            assert.isNumber(res[0].likes, "Likes should be a number")
+            assert.equal(res[0].likes, 1)
+            done();
+        });
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({symbolone: 'ge', like: true})
+        .end(function(err, res){
+            assert.equal(res[0].symbol, "ge");
+            assert.isString(res[0].symbol, "Symbol one should be a string");
+            assert.isNumber(res[0].price, "Price should be a number");
+            assert.isNumber(res[0].likes, "Likes should be a number")
+            assert.equal(res[0].likes, 2)
+            done();
+        });
       });
       
       test('2 stocks', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({symbolone: 'msft', symboltwo: 'face', like: false})
+        .end(function(err, res){
+            assert.equal(res[0].symbol, "msft");
+            assert.equal(res[1].symbol, "face")
+            assert.isString(res[0].symbol, "Symbol should be a string");
+            assert.isString(res[1].symbol, "Symbol should be a string")
+            assert.isNumber(res[0].price, "Price should be a number");
+            done();
+        });        
       });
       
       test('2 stocks with like', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({symbolone: 'msft', symboltwo: 'face', like: true})
+        .end(function(err, res){
+            assert.equal(res[0].symbol, "msft");
+            assert.equal(res[1].symbol, "face")
+            assert.isString(res[0].symbol, "Symbol should be a string");
+            assert.isString(res[1].symbol, "Symbol should be a string")
+            assert.isNumber(res[0].price, "Price should be a number");
+            assert.isNumber(res[0].likes, "Likes should be a number");
+            assert.isNumber(res[1].likes, "Likes should be a number")
+            assert.equal(res[0].likes, 1);
+            assert.equal(res[1].likes, 1);
+            done();
+        });
       });
       
     });
